953eea4b95ff2bc0616cd9ccf8ef67f0cadbead1
FreddyClean <freddyclean55@gmail.com>
2026-06-01 05:21:14 +0700

    Change health to ping to avoid ad blockers

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.use(
 app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
 
-app.get('/health', (_req, res) => res.json({ ok: true }));
+app.get('/ping', (_req, res) => res.json({ ok: true }));
 
 app.use('/auth', authRouter);
 app.use('/years', yearsRouter);
